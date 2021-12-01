b704852b6055d60ee03c9a6fcf4aca00fab748ae
1984204066 <1984204066@qq.com>
2021-12-01 19:22:07 +0800

    added #title

--- blog-assets/src/main.rs
+++ blog-assets/src/main.rs
@@ -1,5 +1,5 @@
 use nipper::Document;
-use std::{fs::*, io::Write};
+use std::{fs::*, io::Write, str::Chars};
 use walkdir::WalkDir;
 use regex::Regex;
 use urlencoding::decode;
@@ -20,8 +20,8 @@ fn main() {
 	    let re = Regex::new(r"(?P<m>\d+)\.(?P<d>\d+)\.(?P<year>\d{4})").unwrap();
 	    let caps = re.captures(&date).unwrap();
 	    let date= caps.name("year").unwrap().as_str().to_string() +
-		"-" + caps.name("m").unwrap().as_str() +
-		"-" + caps.name("d").unwrap().as_str();
+		"-" + format!("{:0>2}", caps.name("m").unwrap().as_str()).as_str() +
+		"-" + format!("{:0>2}", caps.name("d").unwrap().as_str()).as_str();
 
 	    let article  = document.select("div.txtcont");
 	    let title = decode(&title).unwrap();
@@ -29,14 +29,50 @@ fn main() {
 	    let title = re.replace_all(&title, "");
 	    let title = title.replace("&nbsp;", "");
 	    // println!("{}\n{}\n{}", title, date, article.html());
-	    println!("{}\n{}", title, date);
+	    // println!("{}\n{}", title, date);
 
-	    let ofile = "./part-html/".to_string() + &date + "-" +  &title;
+	    let ofile = "".to_string() + &date + "-" +  &title;
 	    println!("{}", &ofile);
+	    let html = article.html();
 	    {
-		let mut ofile = std::fs::File::create(ofile).unwrap();
-		ofile.write_all(article.html().as_bytes());
+		let mut art = std::fs::File::create("./part-html/".to_string() + &ofile).unwrap();
+		art.write_all(html.as_bytes());
+		// let html = html.chars();
+		let tag=  get_tag(&html.chars());
+		let prefix = format!("---
+layout: post
+title:  {}
+date:   {}
+tags:
+      - {}
+---\n\n#{}\n\n", title, date, tag, title);
+		let mut top = std::fs::File::create("./markdown/".to_string() + &ofile).unwrap();
+
+		top.write_all(prefix.as_bytes());
 	    }
 
 	}
+}
+
+fn get_tag(html: &Chars) -> String {
+    let re = Regex::new(r"['佛' '僧' '禅']").unwrap();
+
+    // if html.find(|c| {
+    // 	let i = *c as u32;
+    // 	let keys = ['佛', '僧', '禅'];
+    // 	keys.iter().find_map(|s| {
+    // 	    let b=  *s as u32;
+    // 	    i == b}).is_some()
+    // 	// let b = '佛' as u32;
+    // 	// i == b
+    // }) {
+    // 	tag = "佛法";
+    // } else if html.find("医") {
+    // 	tag = "中医";
+    // } else if html.find("") {
+    // 	tag = "it";
+    // } else {
+    // 	tag = "随笔";
+    // }
+    "随笔".to_string()
 }
